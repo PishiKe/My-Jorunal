ba864cca515b07d509fa15e5cee631dab7353ece
PishiKe <nashpeter33@gmail.com>
2021-10-16 19:00:43 +0300

    Update DiaryDatabase.kt

--- app/src/main/java/com/pishi/mydiary/model/database/DiaryDatabase.kt
+++ app/src/main/java/com/pishi/mydiary/model/database/DiaryDatabase.kt
@@ -1,6 +1,8 @@
 package com.pishi.mydiary.model.database
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.pishi.mydiary.model.entities.MyDiary
 
@@ -8,4 +10,28 @@ import com.pishi.mydiary.model.entities.MyDiary
 
 abstract class DiaryDatabase : RoomDatabase() {
 
+    abstract fun myDiaryDao() : MyDiaryDao
+
+    companion object{
+        @Volatile
+        private var INSTANCE : DiaryDatabase? = null
+
+        fun getDatabase(context : Context) : DiaryDatabase{
+            //if the INSTANCE is not null, return the database
+            //if the INSTANCE is null, create the database
+            return INSTANCE?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    DiaryDatabase::class.java,
+                    "fav_dish_database"
+                )
+                    .fallbackToDestructiveMigration()
+                    .build()
+                INSTANCE = instance
+                // return instance
+
+                instance
+            }
+        }
+    }
 }
