456c1403682952cf1dad1a5f0db1e0af3cfac7f6
PishiKe <nashpeter33@gmail.com>
2021-10-19 13:09:17 +0300

    Permission Updates and Dexter Implimentation

--- app/src/main/java/com/pishi/mydiary/view/activities/DiaryEntry.kt
+++ app/src/main/java/com/pishi/mydiary/view/activities/DiaryEntry.kt
@@ -1,8 +1,12 @@
 package com.pishi.mydiary.view.activities
 
+import android.Manifest
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.karumi.dexter.Dexter
 import com.pishi.mydiary.databinding.ActivityDiaryEntryBinding
+import com.pishi.mydiary.databinding.ImageSelectionDialogBinding
 
 class DiaryEntry : AppCompatActivity() {
 
@@ -13,5 +17,25 @@ class DiaryEntry : AppCompatActivity() {
 
         binding = ActivityDiaryEntryBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.ivImageSelection.setOnClickListener {
+
+        }
+    }
+
+    private fun imageSelectionDialog(){
+
+        val dialog = Dialog(this)
+        val binding : ImageSelectionDialogBinding = ImageSelectionDialogBinding.inflate(layoutInflater)
+
+        dialog.setContentView(binding.root)
+
+        binding.ivLaunchCamera.setOnClickListener {
+
+            Dexter.withContext(this).withPermission(
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+            )
+        }
+
     }
 }
