4933613f990a96914c5cae98ccbbd241f25c73d0
PishiKe <nashpeter33@gmail.com>
2021-10-20 07:08:02 +0300

    Dexter Implimentation

--- app/src/main/java/com/pishi/mydiary/view/activities/DiaryEntry.kt
+++ app/src/main/java/com/pishi/mydiary/view/activities/DiaryEntry.kt
@@ -2,9 +2,19 @@ package com.pishi.mydiary.view.activities
 
 import android.Manifest
 import android.app.Dialog
+import android.content.Intent
+import android.hardware.Camera
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import com.karumi.dexter.Dexter
+import com.karumi.dexter.MultiplePermissionsReport
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionDeniedResponse
+import com.karumi.dexter.listener.PermissionGrantedResponse
+import com.karumi.dexter.listener.PermissionRequest
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener
+import com.karumi.dexter.listener.single.PermissionListener
 import com.pishi.mydiary.databinding.ActivityDiaryEntryBinding
 import com.pishi.mydiary.databinding.ImageSelectionDialogBinding
 
@@ -32,10 +42,64 @@ class DiaryEntry : AppCompatActivity() {
 
         binding.ivLaunchCamera.setOnClickListener {
 
-            Dexter.withContext(this).withPermission(
+            Dexter.withContext(this).withPermissions(
                 Manifest.permission.READ_EXTERNAL_STORAGE,
-            )
+                Manifest.permission.CAMERA
+            ).withListener(object : MultiplePermissionsListener{
+                override fun onPermissionsChecked(report: MultiplePermissionsReport?) {
+                    report?.let {
+                        if (report.areAllPermissionsGranted()){
+                            val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                            startActivityForResult(intent, CAMERA)
+                        } else{
+                            showRationaleDialogForPermission()
+                        }
+                    }
+                }
+
+                override fun onPermissionRationaleShouldBeShown(
+                    permissions: MutableList<PermissionRequest>?,
+                    token: PermissionToken?
+                ) {
+                    showRationaleDialogForPermission()
+                }
+
+            }).onSameThread().check()
+
+            dialog.dismiss()
         }
 
+        binding.ivLaunchGallery.setOnClickListener {
+
+            Dexter.withContext(this).withPermission(
+                Manifest.permission.READ_EXTERNAL_STORAGE
+            ).withListener(object : PermissionListener{
+                override fun onPermissionGranted(p0: PermissionGrantedResponse?) {
+                    TODO("Not yet implemented")
+                }
+
+                override fun onPermissionDenied(p0: PermissionDeniedResponse?) {
+                    TODO("Not yet implemented")
+                }
+
+                override fun onPermissionRationaleShouldBeShown(
+                    p0: PermissionRequest?,
+                    p1: PermissionToken?
+                ) {
+                    TODO("Not yet implemented")
+                }
+
+            })
+        }
+
+    }
+
+    private fun showRationaleDialogForPermission(){
+
+    }
+
+    companion object{
+
+        private const val CAMERA = 1
     }
 }
