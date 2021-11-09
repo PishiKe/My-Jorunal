18293bbc92ad2ade0d2ab1b7982badfb018e13a2
PishiKe <nashpeter33@gmail.com>
2021-11-09 13:51:31 +0300

    Update SplashScreen.kt

--- app/src/main/java/com/pishi/mydiary/view/activities/SplashScreen.kt
+++ app/src/main/java/com/pishi/mydiary/view/activities/SplashScreen.kt
@@ -1,7 +1,10 @@
 package com.pishi.mydiary.view.activities
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowInsets
+import android.view.WindowManager
 import com.pishi.mydiary.R
 import com.pishi.mydiary.databinding.ActivitySplashScreenBinding
 
@@ -12,5 +15,16 @@ class SplashScreen : AppCompatActivity() {
 
         val splashScreenBinding = ActivitySplashScreenBinding.inflate(layoutInflater)
         setContentView(splashScreenBinding.root)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R){
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else{
+            @Suppress("DEPRECATION")
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+
     }
 }
