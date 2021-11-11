274d928cf565bd15653a789dc380a66c3ab81d7e
PishiKe <nashpeter33@gmail.com>
2021-11-11 05:26:24 +0300

    splash screen implimentation

--- app/src/main/java/com/pishi/mydiary/view/activities/SplashScreen.kt
+++ app/src/main/java/com/pishi/mydiary/view/activities/SplashScreen.kt
@@ -1,10 +1,15 @@
 package com.pishi.mydiary.view.activities
 
+import android.content.Intent
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.WindowInsets
 import android.view.WindowManager
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import com.pishi.mydiary.R
 import com.pishi.mydiary.databinding.ActivitySplashScreenBinding
 
@@ -26,5 +31,25 @@ class SplashScreen : AppCompatActivity() {
             )
         }
 
+        val splashScreenAnimation = AnimationUtils.loadAnimation(this, R.anim.fade_out)
+        splashScreenBinding.tvMyDiary.animation = splashScreenAnimation
+
+        splashScreenAnimation.setAnimationListener(object : Animation.AnimationListener{
+            override fun onAnimationStart(animation: Animation?) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onAnimationEnd(animation: Animation?) {
+                Handler(Looper.getMainLooper()).postDelayed({
+                    startActivity(Intent(this@SplashScreen, MainActivity::class.java))
+                    finish()
+                },1000)
+            }
+
+            override fun onAnimationRepeat(animation: Animation?) {
+                TODO("Not yet implemented")
+            }
+
+        })
     }
 }
